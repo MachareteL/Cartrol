af21bbb2c71ca891538a0b92ed9d716535812941
MachareteL <macharetelucas@gmail.com>
2023-08-24 16:53:22 -0300

    rechart lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bermuda: "#0400C2",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
