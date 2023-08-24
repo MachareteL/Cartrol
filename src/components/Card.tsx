c80e5c6090fd18759bfb873de4e934670fd48833
MachareteL <macharetelucas@gmail.com>
2023-08-24 16:53:41 -0300

    Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+export default function Card({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      className={`relative rounded-xl bg-white p-4 before:absolute before:-top-1 before:left-0 before:-z-10 before:h-6 before:w-full before:rounded-t-xl before:bg-bermuda w-fit`}
+    >
+      {children}
+    </div>
+  );
+}
